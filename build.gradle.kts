c31afdec0675272c306948ce3fb4138e2563e5a9
Vootele Rotov <vootele.rotov@gmail.com>
2024-01-26 09:21:56 +0200

    Implementation: add test coverage for creating token from a stoken token
    Covers the whole flow.
    
    The expected values generated from stoken using 'stoken --use-time'.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.1")
+    testImplementation("org.assertj:assertj-core:3.25.1")
 }
 
 tasks.test {
